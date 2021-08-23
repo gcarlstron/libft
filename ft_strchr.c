697390eec72c36f252e780567101a3bfee3e0ec7
Gustavo Pacheco Padre Carlstron <gustavo.carlstron@gmail.com>
2021-08-23 17:47:45 -0300

    correcting ft_strchr and ft_strrchr to return a pointer

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,20 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 12:43:52 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/08/23 16:39:19 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/08/23 17:45:57 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strchr(const char *s, int c)
+char * ft_strchr(const char *s, int c)
 {
 	int i;
-
+	char *p;
+	
+	p = 0;
 	i = 0;
 	while(s[i] != '\0' && s[i] != c)
 		i++;
 	if(s[i] == c)
-		return &s[i];
-	return 0;
+		*p = *s+i;
+	return p;
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 16:36:25 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/08/23 16:40:03 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/08/23 17:45:57 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 char *ft_strrchr(const char *s, int c)
 {
 	int i;
+	char *p;
 
-	i = ft_strlen(s);
+	p = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	while(i > 0 && s[i] != c)
 		i--;
 	if(s[i] == c)
-		return &s[i];
-	return 0;
+		*p = *s+i;
+	return p;
 }
