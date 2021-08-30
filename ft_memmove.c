737754b42cee2fe99c1a7af72c6ccdc3e8448d62
Gustavo Pacheco Padre Carlstron <gustavo.carlstron@gmail.com>
2021-08-30 14:14:17 -0300

    Update ft_memmove.c
    remove malloc

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,20 +6,18 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 12:20:33 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/08/30 12:35:48 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/08/30 13:57:29 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char *tmp;
+	unsigned char		cdst;
+	const unsigned char	csrc;
+	size_t				i;
 
-	tmp = (char *)malloc(sizeof(char) * len);
-	if(tmp == NULL)
-		return (NULL);
-	ft_memcpy(tmp, src, len);
-	ft_memcpy(dst, tmp, len);
-	free(tmp);
-	return(dst);
+	cdst = (unsigned char)dst;
+	csrc = (unsigned char)src;
+	i = 0;
 }
