fac8131b37ec4d0224f5213389242d13b0ab4e34
Gustavo Pacheco Padre Carlstron <gustavo.carlstron@gmail.com>
2021-09-01 14:47:29 -0300

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 14:42:17 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/08/30 16:25:09 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/09/01 14:19:54 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
 {
 	size_t i;
+	char * s;
+
+	s = (char *)src;
 
 	i = 0;
 	if (dstsize == 0)
-		return (ft_strlen(src));
+		return (ft_strlen(s));
 	while(i < dstsize - 1 && src[i])
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (ft_strlen(s));
 }
