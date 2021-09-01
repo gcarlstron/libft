9c2276cdf6451ab5557c8fe905de3c8ae8ba1fbb
Gustavo Pacheco Padre Carlstron <gustavo.carlstron@gmail.com>
2021-09-01 14:14:34 -0300

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 19:22:59 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/08/30 14:32:04 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/09/01 14:13:54 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 void	*ft_memset(void *s, int c, size_t len)
 {
 	char	*str;
+	size_t i;
 
 	str = (char *)s;
-	while (len-- < 0)
+	i = 0;
+	while (i < len)
 	{
-		*str++ = c;
-		len--;
+		str[i] = c;
+		i++;
 	}
 	return (s);
 }
